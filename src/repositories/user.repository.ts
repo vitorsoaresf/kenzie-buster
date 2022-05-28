c53441e2628197d3521cdd9c18559b5c916cba44
Vitor Soares Ferreira <vitorsoaresf@gmail.com>
2022-05-28 08:46:54 -0300

    feat: implements UserRepository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,27 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../data-source";
+import { User } from "../entities/user.entity";
+
+interface IUserRepo {
+  save: (user: Partial<User>) => Promise<User>;
+  all: () => Promise<User[]>;
+  findOne: (payload: object) => Promise<User>;
+//   findOne: (payload: object) => Promise<User>;
+}
+
+class UserRepo implements IUserRepo {
+  private ormRepo: Repository<User>;
+
+  constructor() {
+    this.ormRepo = AppDataSource.getRepository(User);
+  }
+
+  save = async (user: Partial<User>) => await this.ormRepo.save(user);
+  all = async () => await this.ormRepo.find();
+
+  findOne = async (payload: object) => {
+    return await this.ormRepo.findOneBy({ ...payload }) || {} as User;
+  };
+}
+
+export default new UserRepo();
