d21b9ef5394384cbb91b6fba9fa33051b62a7ef8
Vitor Soares Ferreira <vitorsoaresf@gmail.com>
2022-05-30 21:48:05 -0300

    fix: refactore post to put

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -5,6 +5,6 @@ import validateToken from "../middlewares/validateToken.middleware";
 const cartRouter = Router();
 
 cartRouter.get("", cartController.ALL_CARTS);
-cartRouter.post("/pay", validateToken, cartController.CART_PAY);
+cartRouter.put("/pay", validateToken, cartController.CART_PAY);
 
 export default cartRouter;
